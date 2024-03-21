41361ff3ea52723b43e5d82d688c796a98ab404f
deewe <mr.denislam@yandex.ru>
2024-03-21 22:46:24 +0500

    new func for str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def str_func(word):
+    """возвращает строку с заглавными буквами """
     return word.upper()
+
+def str_func_1(word):
+    """возвращает строку с  заглавной первой буквой"""
+    return word.title()
 
